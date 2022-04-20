5c2bd6e4d32eef23e29db5434be097d0cb499cdc
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-04-20 14:49:20 -0500

    making the boxes in an array

--- sketch.js
+++ sketch.js
@@ -1,12 +1,16 @@
 let count;
-let b;
+let b = []
 function setup() {
   createCanvas(400, 400);
   count = 1;
-  b= new Box(200,200, 50,50,"x","#2196F3");
-}
+  for(i=0; i<5; i++){
+     b.push(new Box(200,200, 50,50,"x","#2196F3"));
+  }//end loop
+}//end setup
 
 function draw() {
   background(220);
-  b.display()
-}
+  for(i=0; i<5; i++){
+    b[i].display()
+  }//end loop
+}//end draw
