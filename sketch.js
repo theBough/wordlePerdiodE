46b63f90a20397d6994723c96b5f7e74b3ff825f
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-04-20 15:00:31 -0500

    There should now be 5 boxes

--- sketch.js
+++ sketch.js
@@ -4,7 +4,7 @@ function setup() {
   createCanvas(400, 400);
   count = 1;
   for(i=0; i<5; i++){
-     b.push(new Box(200,200, 50,50,"x","#2196F3"));
+     b.push(new Box(10+i*55,200, 50,50,"x","#2196F3"));
   }//end loop
 }//end setup
 
