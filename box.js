ea120858a474db570cec07926286926dba688569
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-04-21 14:55:30 -0500

    Added a letter

--- box.js
+++ box.js
@@ -1,4 +1,4 @@
-function Box(x,y,w,h, letter){
+function Box(x,y,w,h,letter){
   this.x = x;
   this.y = y;
   this.w = w;
@@ -6,9 +6,12 @@ function Box(x,y,w,h, letter){
   this.letter = letter
   
   this.display = function(){
-   
+    //bottom  
+    fill("black")
     rect(this.x, this.y, this.w, this.h);
-     text(this.letter, this.x, this.y);
+    textSize(50)
+    fill("white")
+    text(this.letter, this.x+10, this.y+35)
   }//end display
   
 }//end Box
