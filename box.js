c3489a1f4da31a0ae9291b280051d38874e09529
corbin Boughen <50217007+theBough@users.noreply.github.com>
2022-04-21 14:43:13 -0500

    Letter should show up

--- box.js
+++ box.js
@@ -1,14 +1,14 @@
-function Box(x,y,w,h,letter,col){
+function Box(x,y,w,h, letter){
   this.x = x;
   this.y = y;
   this.w = w;
   this.h = h;
-  this.letter = letter;
-  this.col = col;
+  this.letter = letter
   
   this.display = function(){
-    fill(this.col)
+   
     rect(this.x, this.y, this.w, this.h);
+     text(this.letter, this.x, this.y);
   }//end display
   
-}//end BOX
+}//end Box
